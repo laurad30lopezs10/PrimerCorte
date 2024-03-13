7a76137e7f2a1cea03c24495ba2c45aa66dc416f
Laura <laura.daniela.lopez.3010@gmail.com>
2024-03-13 17:13:29 -0500

    se agrego boton para terminar el registro y se muestra cual fue el registro guardado

--- app/src/main/java/Programacion/primercorte/MainActivity.java
+++ app/src/main/java/Programacion/primercorte/MainActivity.java
@@ -2,6 +2,7 @@ package Programacion.primercorte;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
@@ -9,6 +10,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
    private EditText EmailAddress;
    private CheckBox EmailCheck;
    private TextView Crearpassword;
+   private TextView guardarEmail;
+   private TextView guardasPassword;
+private EditText passwordcreado;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +40,37 @@ public class MainActivity extends AppCompatActivity {
         System.out.println(TextoEmail.getText().toString());
         TextoEmail.setText("Enter your e-mail address");
 
+        guardarEmail = ( TextView) findViewById(R.id.showemail);
+        guardarEmail.setTextSize( 30);
+        guardarEmail.setTextColor(Color.RED);
+        System.out.println(guardarEmail.getText().toString());
+        guardarEmail.setText("There are no registered emails");
+
+
+        guardasPassword = ( TextView) findViewById(R.id.ShowPassword);
+        guardasPassword .setTextSize( 30);
+        guardasPassword .setTextColor(Color.RED);
+        System.out.println(guardasPassword .getText().toString());
+        guardasPassword .setText("There are no password created");
+
+        passwordcreado=(EditText) findViewById(R.id.password);
+        passwordcreado.addTextChangedListener((new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Crearpassword.setTextColor(Color.YELLOW);
+            }
+        }));
+
         EmailAddress=(EditText) findViewById(R.id.EmailAddress);
         EmailAddress.addTextChangedListener(new TextWatcher() {
             @Override
@@ -48,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                TextoEmail.setTextColor(Color.GREEN);
+                TextoEmail.setTextColor(Color.YELLOW);
             }
     });
 
@@ -66,6 +103,21 @@ EmailCheck.setOnClickListener(new View.OnClickListener() {
         Crearpassword.setText("Create a password");
 
 
+
         }
+        public void cambiarColor(View V){
+            TextoEmail.setTextColor(Color.GREEN);
+
+        }
+    public void Verpassword(View V){
+        Crearpassword.setTextColor(Color.GREEN);
+    }
+public void TerminarRegistro(View v){
+    guardarEmail.setText(EmailAddress.getText().toString());
+    guardarEmail.setTextColor(Color.GREEN);
+    guardasPassword.setText(passwordcreado.getText().toString());
+    guardasPassword.setTextColor(Color.GREEN);
+}
+
     }
 
