7d007abd02c43a5468b73a10b554ab868e86a9e4
Laura <laura.daniela.lopez.3010@gmail.com>
2024-03-13 21:15:18 -0500

    Se agrego menu con items y se cambiaron los colores

--- app/src/main/java/Programacion/primercorte/MainActivity.java
+++ app/src/main/java/Programacion/primercorte/MainActivity.java
@@ -7,11 +7,14 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
    private TextView guardarEmail;
    private TextView guardasPassword;
 private EditText passwordcreado;
+private ImageButton MenuOpcionesPlus;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,26 +34,26 @@ private EditText passwordcreado;
         setContentView(R.layout.activity_main);
         TextoRegistro = ( TextView) findViewById(R.id.TextoRegistro);
         TextoRegistro.setTextSize( 30);
-        TextoRegistro.setTextColor(Color.BLUE);
+        TextoRegistro.setTextColor(Color.MAGENTA);
         System.out.println(TextoRegistro.getText().toString());
         TextoRegistro.setText("Register below");
 
         TextoEmail = ( TextView) findViewById(R.id.TextoEmail);
         TextoEmail.setTextSize( 30);
-        TextoEmail.setTextColor(Color.RED);
+        TextoEmail.setTextColor(Color.MAGENTA);
         System.out.println(TextoEmail.getText().toString());
         TextoEmail.setText("Enter your e-mail address");
 
         guardarEmail = ( TextView) findViewById(R.id.showemail);
         guardarEmail.setTextSize( 30);
-        guardarEmail.setTextColor(Color.RED);
+        guardarEmail.setTextColor(Color.MAGENTA);
         System.out.println(guardarEmail.getText().toString());
         guardarEmail.setText("There are no registered emails");
 
 
         guardasPassword = ( TextView) findViewById(R.id.ShowPassword);
         guardasPassword .setTextSize( 30);
-        guardasPassword .setTextColor(Color.RED);
+        guardasPassword .setTextColor(Color.MAGENTA);
         System.out.println(guardasPassword .getText().toString());
         guardasPassword .setText("There are no password created");
 
@@ -98,11 +102,17 @@ EmailCheck.setOnClickListener(new View.OnClickListener() {
 });
         Crearpassword = ( TextView) findViewById(R.id.textopassword);
         Crearpassword.setTextSize( 30);
-        Crearpassword.setTextColor(Color.RED);
+        Crearpassword.setTextColor(Color.MAGENTA);
         System.out.println(Crearpassword.getText().toString());
         Crearpassword.setText("Create a password");
 
-
+MenuOpcionesPlus=(ImageButton)findViewById(R.id.MenuOpciones);
+MenuOpcionesPlus.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        popupMenuAcciones();
+    }
+});
 
         }
         public void cambiarColor(View V){
@@ -117,6 +127,19 @@ public void TerminarRegistro(View v){
     guardarEmail.setTextColor(Color.GREEN);
     guardasPassword.setText(passwordcreado.getText().toString());
     guardasPassword.setTextColor(Color.GREEN);
+}
+
+public void popupMenuAcciones(){
+    PopupMenu pop = new PopupMenu(this,MenuOpcionesPlus);
+    pop.getMenuInflater().inflate(R.menu.menopopup, pop.getMenu());
+    pop.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            Toast.makeText(getBaseContext(),item.getTitle().toString(),Toast.LENGTH_LONG).show();
+            return false;
+        }
+    });
+    pop.show();
 }
 
     }
