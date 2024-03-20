525d3a7d87f6df33863c86143fe17d462bf866b7
Laura <laura.daniela.lopez.3010@gmail.com>
2024-03-20 15:35:34 -0500

    registrar el rango de edad que tiene la persona y se pusieron los image button para seleccionar que zona humeda quieren

--- app/src/main/java/Programacion/primercorte/MainActivity2.java
+++ app/src/main/java/Programacion/primercorte/MainActivity2.java
@@ -2,13 +2,28 @@ package Programacion.primercorte;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
-
+private  RadioGroup Edades;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
     }
-}
+    public void comprobarModoPago(View view) {
+        if (Edades.getCheckedRadioButtonId() == R.id.radioMenores) {
+            final String text = "Recuerda venir con el acompañamiento de un adulto";
+            Toast.makeText(this, text, Toast.LENGTH_LONG).show();}}
+
+    }
+
