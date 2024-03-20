50637253cef92cab365cc235beb995f6ee720395
Laura <laura.daniela.lopez.3010@gmail.com>
2024-03-20 18:38:09 -0500

    pagina 4 para el boton de masajes

--- app/src/main/java/Programacion/primercorte/MainActivity3.java
+++ app/src/main/java/Programacion/primercorte/MainActivity3.java
@@ -11,17 +11,18 @@ import android.widget.TextView;
 public class MainActivity3 extends AppCompatActivity {
 
     private CheckBox piscina;
-    private TextView piscinatext;
+    private TextView Reglasenpiscina;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-TextView Piscinatext=(TextView) findViewById(R.id.ReglasPrincipalText);
+
+Reglasenpiscina=(TextView) findViewById(R.id.ReglasPrincipalText);
         piscina=(CheckBox)findViewById(R.id.checkBoxReglas);
         piscina.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                piscinatext.setTextColor(Color.GREEN);
+                Reglasenpiscina.setTextColor(Color.GREEN);
             }
         });
     }
--- app/src/main/java/Programacion/primercorte/MainActivity4.java
+++ app/src/main/java/Programacion/primercorte/MainActivity4.java
@@ -1,13 +1,26 @@
 package Programacion.primercorte;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity4 extends AppCompatActivity {
-
+private Switch ComprobarMasajesR;
+private TextView MensajeMasajes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+        ComprobarMasajesR=(Switch) findViewById(R.id.SwicthReglas);
+        MensajeMasajes=(TextView)findViewById(R.id.TextoMasajesRelas);
+
     }
+    public void SwichColor(View v){
+        MensajeMasajes.setTextColor(Color.GREEN);
+    }
+
 }
