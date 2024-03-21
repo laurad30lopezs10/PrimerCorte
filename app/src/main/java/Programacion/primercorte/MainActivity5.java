abb07852b72b69d4c9582381a3dc6852c0fb794e
Laura <laura.daniela.lopez.3010@gmail.com>
2024-03-20 19:31:16 -0500

    se realizo el ultimo mainActivity para los botones de cada una de las opciones de zonas humedas

--- app/src/main/java/Programacion/primercorte/MainActivity5.java
+++ app/src/main/java/Programacion/primercorte/MainActivity5.java
@@ -2,13 +2,26 @@ package Programacion.primercorte;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity5 extends AppCompatActivity {
 
+    private RadioButton verificarRglas;
+    private TextView ReglasSauna;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+        verificarRglas=(RadioButton) findViewById(R.id.radioButtonSauna);
+        ReglasSauna=(TextView) findViewById(R.id.TextoReglasSauna);
+    }
+    public void ButtonColor(View v){
+        ReglasSauna.setTextColor(Color.GREEN);
     }
 }
